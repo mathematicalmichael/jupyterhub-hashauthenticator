d920279c0c411aa6f57df90d7d4f2a9be2cc8b7d
Robert Schroll <rschroll@gmail.com>
2017-12-26 00:08:27 -0800

    Move scripts to separate directory
    Also, adjust imports so that they actually work.  The
    password-generation script is renamed to disambiguate it from the
    module.
    
    Additionally, __all__ should be a list of strings, not objects.

--- hashauthenticator/__init__.py
+++ hashauthenticator/__init__.py
@@ -1,4 +1,11 @@
-from hashauthenticator.hashauthenticator import HashAuthenticator
-from hashauthenticator.hashauthenticator import generate_password_digest
+from .passwordhash import generate_password_digest
+# To let the password script be run where JupyterHub isn't installed:
+try:
+  from .hashauthenticator import HashAuthenticator
+except ImportError:
+  print("Warning: Unable to import HashAuthenticator.\n"
+        "Only generate_password_digest will be available.")
+  HashAuthenticator = None
 
-__all__ = [HashAuthenticator, generate_password_digest]
+
+__all__ = ['HashAuthenticator', 'generate_password_digest']
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     url='https://github.com/pminkov/jupyterhub-hashauthenticator',
     author='Petko Minkov',
     author_email='pminkov@gmail.com',
-    scripts=['hashauthenticator/hashauthenticator'],
+    scripts=['scripts/hashauthpw', 'scripts/hashauthservice'],
     install_requires=['jupyterhub'],
     test_suite="hashauthenticator.tests",
     license='BSD3',
