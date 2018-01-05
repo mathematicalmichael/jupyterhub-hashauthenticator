c4308c225e21983c62573d22b7d46e1da97c5c8f
Robert Schroll <rschroll@gmail.com>
2018-01-04 17:23:17 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-hashauthenticator',
-    version='0.1.2',
+    version='0.2.0',
     description='Hashed Password Authenticator for JupyterHub',
-    url='https://github.com/pminkov/jupyterhub-hashauthenticator',
+    url='https://github.com/thedataincubator/jupyterhub-hashauthenticator',
     author='Petko Minkov',
     author_email='pminkov@gmail.com',
+    maintainer='Robert Schroll',
+    maintainer_email='robert@thedataincubator.com',
     scripts=['scripts/hashauthpw', 'scripts/hashauthservice'],
     install_requires=['jupyterhub'],
     test_suite="hashauthenticator.tests",
